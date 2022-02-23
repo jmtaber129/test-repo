3566f8655ab4bb1512f673e596c4460de28d3c60
James Taber <jmtaber129@gmail.com>
2022-02-22 22:19:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function onTrack(event, settings) {
   console.log(event);
   console.log(settings);
 }
+
+function onRequest(request) {
+  const body = request.json()
+  console.log('Hello', body.name)
+  Freshpaint.track({
+    event: 'Request received',
+    userId: body.name,
+    properties: {
+      foo: 'bar'
+    }
+  })
+}
