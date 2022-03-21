89e877c7a571b43fe1e47c603fd5d4d1a9b9053e
James Taber <jmtaber129@gmail.com>
2022-03-21 16:14:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-function onTrack(event, settings) {
+async function onTrack(event, settings) {
   console.log(event);
   console.log(settings);
-  console.log("Using API key", settings.apiKey);
+  console.log("Using API key", settings.apiKey);  
+  await fetch('https://eonxyz3ivydsdlh.m.pipedream.net', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'API-Key': settings.apiKey
+    },
+    body: JSON.stringify({
+      event_name: event.event,
+      event_properties: event.properties,
+      timestamp: event.timestamp
+    })
+  })
 }
 
 function onRequest(request) {
